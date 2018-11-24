06ef30224261237b3ff8a239aebbc93ba34ed97c
北原杜魚 <kitamori6@yahoo.co.jp>
2018-11-24 16:36:10 +0900

    リスト追加2

---
+++ resources/views/sayings/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div class="container">
+    <div class="flex">
+        <h1 class="header">名言一覧</h1>
+        <div class="btn-box">
+            <a class="btn-flat main-btn" href="{{ action('SayingsController@create', Request::all()) }}">新規登録</a>
+        </div>
+    </div>
+
+        @foreach ($sayings as $saying)
+            
+        
+
+    <table class="company">
+<tbody>
+<tr>
+<th class="arrow_box">名言</div></th>
+<td>{{{ $saying->saying }}}</td>
+</tr>
+<tr>
+<th class="arrow_box">引用元</th>
+<td>
+{{{ $saying->quote }}}
+</td>
+</tr>
+<tr>
+<th>名言にまつわるストーリー</th>
+<td>
+{{{ $saying->content }}}
+</td>
+</tr>
+</tbody>
+</table>
+@endforeach
+    <div class="table-box flex">
+        <div class="scroll-box three">
+        <a class="btn-flat main-btn" href="{{ action('SayingsController@index', Request::all()) }}">一覧へ戻る</a>
+
+        </div>
+    </div>
+</div>
+
+@endsection
